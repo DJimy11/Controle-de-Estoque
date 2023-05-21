30bde04c88ff63229bf93ea580b4e5ed4ffb478a
Arnaldozeki <josearnaldohatta@gmail.com>
2023-05-21 17:31:25 +0100

    Codigo fonte mais profissional

--- ControleDeEstoque/ControleDeEstoque.Web/Scripts/Cadastro/CadBase.js
+++ ControleDeEstoque/ControleDeEstoque.Web/Scripts/Cadastro/CadBase.js
@@ -63,7 +63,10 @@ $(document).on('click', '#btn_incluir', function () {
         if (response) {
             abrir_form(response);
         }
-    });
+    })
+        .fail(function () {
+            swal('Aviso', 'Não foi possivel recuperar as informações.Tente novamente em instantes.', 'warning');
+        });
 })
 .on('click', '.btn-excluir', function () {
     var btn = $(this),
@@ -95,6 +98,9 @@ $(document).on('click', '#btn_incluir', function () {
                             $('#mensagem_grid').removeClass('invisivel');
                         }
                     }
+                })
+                .fail(function () {
+                     swal('Aviso', 'Não foi possivel excluir.Tente novamente em instantes.', 'warning');
                 });
             }
         }
@@ -134,6 +140,9 @@ $(document).on('click', '#btn_incluir', function () {
             $('#msg_mensagem_aviso').show();
             $('#msg_erro').hide();
         }
+    })
+    .fail(function () {
+            swal('Aviso', 'Não foi possivel salvar.Tente novamente em instantes.', 'warning');
     });
 })
 .on('click', '.page-item', function () {
@@ -166,6 +175,9 @@ $(document).on('click', '#btn_incluir', function () {
             btn.siblings().removeClass('active');
             btn.addClass('active');
         }
+    })
+    .fail(function () {
+        swal('Aviso', 'Não foi possivel recuperar as informações.Tente novamente em instantes.', 'warning');
     });
 })
 .on('change', '#ddl_tam_pag', function () {
@@ -198,6 +210,9 @@ $(document).on('click', '#btn_incluir', function () {
             ddl.siblings().removeClass('active');
             ddl.addClass('active');
         }
+    })
+        .fail(function () {
+            swal('Aviso', 'Não foi possivel recuperar as informações.Tente novamente em instantes.', 'warning');
     });
 })
 .on('keyup', '#txt_filtro', function () {
@@ -229,5 +244,8 @@ $(document).on('click', '#btn_incluir', function () {
             ddl.siblings().removeClass('active');
             ddl.addClass('active');
         }
+    })
+    .fail(function () {
+       swal('Aviso', 'Não foi possivel recuperar as informações.Tente novamente em instantes.', 'warning');
     });
 });
